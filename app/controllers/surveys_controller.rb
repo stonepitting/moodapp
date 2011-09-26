f0c155451f40cfeb4c53ce3b2c913b9b8dd7c7ba
Sten Pittet <spittet@atlassian.com>
2011-09-26 14:56:10 +1000

    modifications

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,5 +1,5 @@
 class SurveysController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => :stats
   # GET /surveys
   # GET /surveys.xml
   def index
